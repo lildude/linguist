4a5261c5ec74b7c38035f6af97f6daa5bca90570
Colin Seymour <colin@github.com>
2017-12-09 15:31:55 +0000

    Move func to helper

--- lib/linguist/blob_helper.rb
+++ lib/linguist/blob_helper.rb
@@ -387,3 +387,14 @@ module Linguist
     end
   end
 end
+
+class Integer
+  def to_filesize
+    {
+      'B'  => 1024,
+      'KB' => 1024 * 1024,
+      'MB' => 1024 * 1024 * 1024,
+      'GB' => 1024 * 1024 * 1024 * 1024,
+    }.each_pair { |e, s| return "#{(self.to_f / (s / 1024)).round(2)} #{e}" if self < s }
+  end
+end
